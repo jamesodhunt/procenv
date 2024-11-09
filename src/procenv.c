cbb6269b95f1ec26a49fc04eb03e2eaa739d10d8
James O. D. Hunt <jamesodhunt@gmail.com>
2024-11-09 11:24:31 +0000

    cleanup: Remove duplicated perms code
    Remove the redundant duplicated permissions handling code in
    `show_fds_linux()` and `show_stat()` after calling `format_perms()`
    since that function handles the logic itself.
    
    Signed-off-by: James O. D. Hunt <jamesodhunt@gmail.com>

--- src/procenv.c
+++ src/procenv.c
@@ -1920,13 +1920,6 @@ show_stat (void)
 		die ("failed to allocate space for permissions string");
 	perms = (st.st_mode & ~S_IFMT);
 
-	if (perms & S_ISUID)
-		modestr[3] = 's';
-	if (perms & S_ISGID)
-		modestr[6] = 's';
-	if (perms & S_ISVTX)
-		modestr[9] = 't';
-
 	section_open ("permissions");
 
 	entry ("octal", "%4.4o", perms);
